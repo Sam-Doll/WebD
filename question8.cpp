87f356bb36f361e0f08193977ce9e6fa2569f807
Smriti Gupta <135643404+Sam-Doll@users.noreply.github.com>
2024-08-29 23:30:00 +0530

    added question8.cpp

---
+++ question8.cpp
@@ -0,0 +1,16 @@
+// CRICKET WORLD CUP QUALIFIER 203
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int X;
+    cin >> X;
+    if(X>=12){
+        cout<< "YES";
+    }else{
+        cout<< "NO";
+    }
+    return 0;
+}
