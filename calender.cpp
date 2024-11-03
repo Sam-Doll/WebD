ee284bd3a9a82b0f7ee9a1ce415fdf89a4d8e219
Smriti Gupta <135643404+Sam-Doll@users.noreply.github.com>
2024-11-03 23:56:06 +0530

    added calender.cpp

---
+++ calender.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class MyCalendar {
+public:
+    MyCalendar() {
+        
+    }
+    
+    bool book(int start, int end) {
+        
+    }
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */
